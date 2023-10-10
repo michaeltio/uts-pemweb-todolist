0060a42cf96a34913a52e33a00746eee319ad756
michael tio <77330912+michaeltio@users.noreply.github.com>
2023-10-10 18:18:24 +0700

    update :  bikin dashboard

--- pages/index.php
+++ pages/index.php
@@ -8,10 +8,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/dist/output.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>To do List</title>
 </head>
-<body>
-    <h1 class="text-red-700">Hello</h1>
+<body class="bg-gradient-to-r from-orange-500 to-purple-500">
+    <div class="flex justify-center flex-col items-center h-screen">
+        <h1 class="text-6xl text-center text-white">WELCOME TO PRIORILIST</h1>
+        <p class="text-white text-lg">Stay Organized, Achieve More.</p>
+        <div class="mt-4">
+        <a href="#" class="inline-block px-6 py-3 text-lg font-semibold text-white bg-blue-500 hover:bg-blue-600 rounded-full hover:shadow-lg transition duration-300 ease-in-out mr-4">Register</a>
+            <a href="#" class="inline-block px-6 py-3 text-lg font-semibold text-white bg-green-500 hover:bg-green-600 rounded-full hover:shadow-lg transition duration-300 ease-in-out">Login</a>
+        </div>
+    </div>
 </body>
 </html
