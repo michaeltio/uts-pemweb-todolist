8efea8e6177cf7137d118dc847d9c9af5d967d04
michael tio <77330912+michaeltio@users.noreply.github.com>
2023-10-10 19:09:15 +0700

    update : tambahin tailwind

--- pages/index.php
+++ pages/index.php
@@ -1,6 +1,6 @@
 <?php
     // ini dashboard home page
-   
+
 ?>
 
 <!DOCTYPE html>
@@ -8,10 +8,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="/public/css/app.css">
     <title>To do List</title>
 </head>
-<body class="bg-gradient-to-r from-orange-500 to-purple-500">
+<body class="bg-gradient-to-tr from-orange-500 to-purple-500">
     <div class="flex justify-center flex-col items-center h-screen">
         <h1 class="text-6xl text-center text-white">WELCOME TO PRIORILIST</h1>
         <p class="text-white text-lg">Stay Organized, Achieve More.</p>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',    
+    './**/*.html',     
+    './index.php',     
+    './**/*.php',      
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
