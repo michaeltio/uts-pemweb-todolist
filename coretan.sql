f1642c557c141473fc413fff9a6c72aa6508de0a
michael tio <77330912+michaeltio@users.noreply.github.com>
2023-10-18 19:04:23 +0700

    update : benerin tampilan workspace

--- coretan.sql
+++ coretan.sql
@@ -10,12 +10,56 @@ CREATE TABLE users(
 
 
 CREATE TABLE tasks(
-    id_task    INT AUTO_INCREMENT,
-    username         VARCHAR(20),
+    id_task         INT AUTO_INCREMENT,
+    username        VARCHAR(20),
     title           VARCHAR(30) NOT NULL,
+    task_desc       VARCHAR(255),
+    created         DATE,
     isComplete      BOOLEAN NOT NULL,
     progress        VARCHAR(30) NOT NULL,
 
     PRIMARY KEY(id_task),
     FOREIGN KEY(username) REFERENCES users(username)
 );
+
+--dummy values
+INSERT INTO users (username, password) VALUES
+('admin', 'admin'),
+('rehan', 'guaganteng'),
+('tio', 'puhsepuh');
+
+INSERT INTO tasks (username, title, task_desc, created, isComplete, progress) VALUES
+('admin', 'Task 1', 'Description 1', '2023-10-18', 1, 'In Progress'),
+('admin', 'Task 2', 'Description 2', '2023-10-19', 0, 'Not Started'),
+('admin', 'Task 3', 'Description 3', '2023-10-20', 0, 'Not Started'),
+('admin', 'Task 4', 'Description 4', '2023-10-21', 1, 'Completed'),
+('admin', 'Task 5', 'Description 5', '2023-10-22', 0, 'Not Started'),
+('admin', 'Task 6', 'Description 6', '2023-10-23', 1, 'In Progress'),
+('admin', 'Task 7', 'Description 7', '2023-10-24', 1, 'In Progress'),
+('admin', 'Task 8', 'Description 8', '2023-10-25', 0, 'Not Started'),
+('admin', 'Task 9', 'Description 9', '2023-10-26', 0, 'Not Started'),
+('admin', 'Task 10', 'Description 10', '2023-10-27', 1, 'Completed');
+
+INSERT INTO tasks (username, title, task_desc, created, isComplete, progress) VALUES
+('rehan', 'Task 11', 'Description 11', '2023-10-18', 1, 'In Progress'),
+('rehan', 'Task 12', 'Description 12', '2023-10-19', 0, 'Not Started'),
+('rehan', 'Task 13', 'Description 13', '2023-10-20', 0, 'Not Started'),
+('rehan', 'Task 14', 'Description 14', '2023-10-21', 1, 'Completed'),
+('rehan', 'Task 15', 'Description 15', '2023-10-22', 0, 'Not Started'),
+('rehan', 'Task 16', 'Description 16', '2023-10-23', 1, 'In Progress'),
+('rehan', 'Task 17', 'Description 17', '2023-10-24', 1, 'In Progress'),
+('rehan', 'Task 18', 'Description 18', '2023-10-25', 0, 'Not Started'),
+('rehan', 'Task 19', 'Description 19', '2023-10-26', 0, 'Not Started'),
+('rehan', 'Task 20', 'Description 20', '2023-10-27', 1, 'Completed');
+
+INSERT INTO tasks (username, title, task_desc, created, isComplete, progress) VALUES
+('tio', 'Task 21', 'Description 21', '2023-10-18', 1, 'In Progress'),
+('tio', 'Task 22', 'Description 22', '2023-10-19', 0, 'Not Started'),
+('tio', 'Task 23', 'Description 23', '2023-10-20', 0, 'Not Started'),
+('tio', 'Task 24', 'Description 24', '2023-10-21', 1, 'Completed'),
+('tio', 'Task 25', 'Description 25', '2023-10-22', 0, 'Not Started'),
+('tio', 'Task 26', 'Description 26', '2023-10-23', 1, 'In Progress'),
+('tio', 'Task 27', 'Description 27', '2023-10-24', 1, 'In Progress'),
+('tio', 'Task 28', 'Description 28', '2023-10-25', 0, 'Not Started'),
+('tio', 'Task 29', 'Description 29', '2023-10-26', 0, 'Not Started'),
+('tio', 'Task 30', 'Description 30', '2023-10-27', 1, 'Completed');
--- pages/workspace-page.php
+++ pages/workspace-page.php
@@ -7,13 +7,23 @@
     if (isset($_SESSION['username'])) {
         // User is logged in; allow access to the dashboard
     } else {
-      
         header('Location: ./login-page.php'); // Redirect to the login page
         exit();
     }
 
     $userLogin = Task::getInstance("localhost", "root", "", "todolist");
     $taskList = $userLogin->getData($_SESSION['username']);
+
+    $incompleteTasks = [];
+    $completedTasks = [];
+
+    while ($row = $taskList->fetch_assoc()) {
+        if ($row['isComplete'] == 0) {
+            $incompleteTasks[] = $row;
+        } else {
+            $completedTasks[] = $row;
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -21,8 +31,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/public/css/app.css">
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="/public/css/app.css">
     <title>Workspace | Priorilist</title>
 </head>
 <body class="bg-gradient-to-tr from-orange-500 to-purple-500">
@@ -31,26 +41,61 @@
             <h1 class="font-bold">PrioriList</h1>
             <p class="">Hello, <?= $_SESSION['username'] ?> </p>
         </div>
-        <div class="bg-neutral-50 mx-auto mt-12 p-8 rounded-lg w-96">
-            <h1 class="font-bold">List Kamu Sekarang</h1>
-            <table class="table-auto border-collapse border">
-                <thead>
-                    <tr>
-                        <th class="bg-gray-100 border px-4 py-2">Title</th>
-                        <th class="bg-gray-100 border px-4 py-2">isComplete</th>
-                        <th class="bg-gray-100 border px-4 py-2">Progress</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php while ($row = $taskList->fetch_assoc()) { ?>
-                        <tr class="hover:bg-gray-200">
-                            <td class="border px-4 py-2"><?= "{$row['title']}" ?></td>  
-                            <td class="border px-4 py-2"><?= "{$row['isComplete']}" ?></td>
-                            <td class="border px-4 py-2"><?= "{$row['progress']}" ?></td>
+        <div class="flex flex-wrap md:flex-nowrap">
+            <div class="bg-neutral-50 mx-auto mt-12 p-8 rounded-lg w-1/4">
+                <h1 class="font-bold text-center">List Kamu Sekarang</h1>
+                <table class="table-auto border-collapse border w-full mt-4">
+                    <thead>
+                        <tr>
+                            <th class="bg-gray-100 border px-4 py-2">Task</th>
+                            <th class="bg-gray-100 border px-4 py-2">Done</th>
+                            <th class="bg-gray-100 border px-4 py-2">Progress</th>
                         </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($incompleteTasks as $row) {?>
+                            <tr class="hover:bg-gray-200">
+                                <td class="border px-4 py-2"><?= $row['title'] ?></td>  
+                                <td class="border px-4 py-2">
+                                    <input type="checkbox" <?= $row['isComplete'] ? 'checked' : '' ?> class="">
+                                </td>
+                                <td class="border px-4 py-2">
+                                    <select name="progressDropdown" class="w-full">
+                                        <option value="plantodo">Plan To Do</option>
+                                        <option value="onhold">On Hold</option>
+                                        <option value="inprogress">In Progress</option>
+                                    </select>
+                                </td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+            <div class="bg-neutral-50 mx-auto mt-12 p-8 rounded-lg w-1/4">
+                <h1 class="font-bold text-center">Sudah Selesai</h1>
+                <table class="table-auto border-collapse border w-full mt-4">
+                    <thead>
+                        <tr>
+                            <th class="bg-gray-100 border px-4 py-2">Task</th>
+                            <th class="bg-gray-100 border px-4 py-2">Done</th>
+                            <th class="bg-gray-100 border px-4 py-2">Progress</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($completedTasks as $row) {?>
+                            <tr class="hover:bg-gray-200">
+                                <td class="border px-4 py-2"><?= $row['title'] ?></td>  
+                                <td class="border px-4 py-2">
+                                    <input type="checkbox" <?= $row['isComplete'] ? 'checked' : '' ?>>
+                                </td>
+                                <td class="border px-4 py-2">
+                                    Hapus
+                                </td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </body>
